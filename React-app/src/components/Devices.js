e8917c22e54951cbbf304d5e51e4d11ac7090572
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-03-10 08:56:31 +0530

    class Component based

--- React-app/src/components/Devices.js
+++ React-app/src/components/Devices.js
@@ -1,103 +1,103 @@
+import React, {Component} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+var iotdata=[] ;
+    var  pressure = [];
+    var ph = [];
 
-
-import React from 'react';
-
-
-import { Chart } from 'react-charts'
-var temArrayT = [];
-var temapArrayH = [];
-var time = [];
-var chart =[];
-function Device(){
-    async function Datafetch()
-    {
-     var myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json;");
-    myHeaders.append("Origin", "http://127.0.0.1:5500/");
     
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-        const url="https://india.table.core.windows.net/iotdata?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-03-09T12:20:40Z&st=2020-03-09T04:20:40Z&spr=https&sig=umQtxbs%2FQJCwCU1Vbg5qByv3zSnt5%2FEVdtR1fH9YvL8%3D";
-        let response = await fetch(url,requestOptions);
-        let datafet= await response.json();
-        console.log(datafet);
-datafet.value.forEach(
-  function(dataVal){
-  
-
-if(dataVal['Pressure'] !== 1 || dataVal['Pressure'] !== 2 || dataVal['Ph'] !== 6 ||dataVal['Ph'] !== 7 )
-{
-  temArrayT.push(dataVal['Pressure']);
-  temapArrayH.push(dataVal['Ph']);
-  time.push(dataVal['RowKey']);
-var chartpoint ={
-  x : dataVal['Pressure'],
-  y : dataVal['Ph']
-}
-chart.push(chartpoint);
-  }
-})
-
-    };
-  var tArr = chart.toString();
-    Datafetch();
-    console.log(chart);
-    console.log(tArr);
-    console.log(temapArrayH);
-    console.log(temArrayT);
-    const data = React.useMemo(
-        () =>[  {
-          label: 'Series 1',
-          data:  tArr
-        },
-       
-        ]
+    async function Fetch()
+    {
+      var myHeaders = new Headers();
+      myHeaders.append("Accept", "application/json;");
+      
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+      
+      const url="https://india.table.core.windows.net/iotdata?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-03-10T11:08:12Z&st=2020-03-10T03:08:12Z&spr=https&sig=LMQ5HEnqWV%2FNM%2FB4hp7QztAOCdeb9DacVk9PIs7BVZE%3D";
+      let response = await fetch(url,requestOptions);
+      let datafet = await response.json();
+      datafet.value.forEach((dataVal)=>{
+        pressure.push(dataVal['Pressure']);
+        ph.push(dataVal['Ph']);
         
-      )
-     
-      const axes = React.useMemo(
-        () => [
-          { primary: true, type: 'linear', position: 'bottom' },
-          { type: 'linear', position: 'left' }
-        ],
-        []
-      )
-     
-     
+      })
+    };
+    Fetch();
+    console.log(ph)
+class Chart extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:{
+        
+        labels: pressure,
+        datasets:[
+          {
+            label:'Population',
+            data: ph,
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              
+            ]
+          }
+        ]
+      ,
+      
+        labels: ph,
+        datasets:[
+          {
+            label:'Population',
+            data: pressure,
+          
+          }
+        ]
+      
+      }
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'City'
+  }
+
+  render(){
     return (
-        <div className="container">
-        <canvas id="myCh"></canvas>
-        <div className="row">
-          <div className="col-md-4">
-          <div className="card" style={{width: 18+'rem'}}>
-               <div className="card-body">
-                   <h5 className="card-title">Device</h5>
-                   <p className="card-title">Location : Palakad</p>
-                   <p> Details</p>
-                   <div style={{ width: '100%', height: '200px' }}>
-          <Chart data={data} axes={axes} />
+      <div className="container">
+      <canvas id="myCh"></canvas>
+      <div className="row">
+        <div className="col-md-4">
+        <div className="card" style={{width: 18+'rem'}}>
+             <div className="card-body">
+                 <h5 className="card-title">Device</h5>
+                 <p className="card-title">Location : Palakad</p>
+                 <p> Details</p>
+                
+
         </div>
-          </div>
-                     
+                   
+      </div>
         </div>
-          </div>
-          </div>
-          </div>
-          
-          
-    );
-};
-export default Device;
+        </div>
+        <Line 
+          data={this.state.chartData}
+          options={{
+           
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+        </div>
+    )
+  }
+}
 
-
-
-
-
-
-
-
-
-
+export default Chart;
